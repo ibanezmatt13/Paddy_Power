a6a1d0d0e5c21dc8be83462db8093b07652fc636
Matthew <matty.beckett@btinternet.com>
2013-08-22 15:43:54 +0100

    Update leds.py

--- leds.py
+++ leds.py
@@ -1,33 +1,10 @@
-import piface.pfio as pfio
+Brew
 
-pfio.init()
+Please change this so that it contains the code you developed for the LEDS which we know works.
 
-while True:
+Then just recompile:
     
-    # turn LED pins 1 to 10 on
-    pfio.digital_write(1, 1)
-    pfio.digital_write(2, 1)
-    pfio.digital_write(3, 1)
-    pfio.digital_write(4, 1)
-    pfio.digital_write(5, 1)
-    pfio.digital_write(6, 1)
-    pfio.digital_write(7, 1)
-    pfio.digital_write(8, 1)
-    pfio.digital_write(9, 1)
-    pfio.digital_write(10, 1)
-    
-    time.sleep(0.5) # LEDs stay on for half a second
-    
-    # turn LED pins 1 to 10 off
-    pfio.digital_write(1, 0)
-    pfio.digital_write(2, 0)
-    pfio.digital_write(3, 0)
-    pfio.digital_write(4, 0)
-    pfio.digital_write(5, 0)
-    pfio.digital_write(6, 0)
-    pfio.digital_write(7, 0)
-    pfio.digital_write(8, 0)
-    pfio.digital_write(9, 0)
-    pfio.digital_write(10, 0)
-    
-    time.sleeo(0.5) # LEDs stay off for half a second
+    sudo chmod 755 leds.py
+
+Thanks
+Matt
