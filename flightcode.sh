dfc0e9ca4b4cb41630a678f0bdcfe936f44f7956
Matthew <matty.beckett@btinternet.com>
2013-08-27 20:47:31 +0100

    Update flightcode.sh

--- flightcode.sh
+++ flightcode.sh
@@ -3,6 +3,7 @@
 while [ 1 ]; do # do  forever
 {
 /home/pi/flightcode.py; # execute the main tracker executable
+/home/pi/camera_timelapse.py; # execute the camera timelapse
 sleep 1 # delay for one second
 }
 
