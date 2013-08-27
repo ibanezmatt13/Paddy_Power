342dd525ed45c4916cef433971e2c895db829718
Brewj <github@jamesbreward.co.uk>
2013-08-27 19:10:54 +0100

    Update motor_2.py

--- motor_2.py
+++ motor_2.py
@@ -9,5 +9,7 @@ while x <= 5:
     pfio.digital_write (0,1) # motor out
     print "button pressed - motor out"
     time.sleep(1) # hold time
+    print "slept"
     pfio.digital_write (0,0) # release button
+    print "released"
     x += 1 # increment counter each iteration
