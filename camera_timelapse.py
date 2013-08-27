ed873aefecb3eaf6386a42c1f1935d3e14d6309f
Matthew <matty.beckett@btinternet.com>
2013-08-27 18:55:51 +0100

    Update camera_timelapse.py

--- camera_timelapse.py
+++ camera_timelapse.py
@@ -1,10 +1,11 @@
 # This script takes pictures and stamps them with the date
 import os
-from time import sleep
+import time
+import datetime
 
-while(True): # Variable to ensure time lapse continues indefinitely 
-	import time
-	import datetime
+while True: # Variable to ensure time lapse continues indefinitely 
+	
+	
 	date = datetime.datetime.fromtimestamp(time.time()).strftime("_%Y-%m-%d_%H-%M-%S") # Define Date
 	os.system("raspistill -o images/image" + date + ".jpg") # Run 'take picture' and save with date stamp
-	sleep (30) # Wait time until repeat
+	time.sleep(30) # Wait time until repeat
