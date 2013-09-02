53c6a27a8c4b06b2d1133b84d94884c6a6ee2363
Matthew <matty.beckett@btinternet.com>
2013-09-02 13:24:08 +0100

    Update flightcode.py

--- flightcode.py
+++ flightcode.py
@@ -126,9 +126,11 @@ def read_gps():
             
             time = float(raw_time)
             string = "%06i" % time # creating a string out of time (this format ensures 0 is included at start if any)
-            hours = float(string[0:2])
-            minutes = float(string[2:4])
-            seconds = float(string[4:6])
+            hours = string[0:2]
+            hours = int(hours)
+            hours = hours + 1
+            minutes = string[2:4]
+            seconds = string[4:6]
             hours = hours + 1
             time = str(str(hours) + ':' + str(minutes) + ':' + str(seconds)) # the final time string in form 'hh:mm:ss'
             latitude = convert(lats, northsouth)
